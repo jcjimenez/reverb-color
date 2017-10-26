412639f5b093454c1d411d1abfb3ef60c18b1f3c
jcjimenez <jc.jimenez@microsoft.com>
2017-10-25 22:02:53 -0500

    Cleanup of new predict code.

--- mobilenet/predict.py
+++ mobilenet/predict.py
@@ -10,12 +10,10 @@ import tensorflow as tf
 def load_graph(model_file):
   graph = tf.Graph()
   graph_def = tf.GraphDef()
-
   with open(model_file, "rb") as f:
     graph_def.ParseFromString(f.read())
   with graph.as_default():
     tf.import_graph_def(graph_def)
-
   return graph
 
 MODELS = {}
@@ -33,69 +31,55 @@ def read_tensor_from_image_file(image_url, input_height=128, input_width=128, in
   dims_expander = tf.expand_dims(float_caster, 0)
   resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
   normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-  sess = tf.Session()
-  result = sess.run(normalized)
-  return result
+  with tf.Session() as session:
+    return session.run(normalized)
 
 def load_labels(label_file):
-  label = []
   proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
-  for l in proto_as_ascii_lines:
-    label.append(l.rstrip())
-  return label
+  return [l.rstrip() for l in proto_as_ascii_lines]
 
 def select_family(families):
-    if len(families):
-        return families[0]
-    else:
-        return None
+    return families[0] if len(families) else None
 
 def run_graph_v2(graph, image_tensor, label_path, input_layer="input", output_layer="final_result"):
     input_name = "import/" + input_layer
     output_name = "import/" + output_layer
-    input_operation = graph.get_operation_by_name(input_name);
-    output_operation = graph.get_operation_by_name(output_name);
+    input_operation = graph.get_operation_by_name(input_name)
+    output_operation = graph.get_operation_by_name(output_name)
 
     with tf.Session(graph=graph) as sess:
         results = sess.run(output_operation.outputs[0], {input_operation.outputs[0]: image_tensor})
     results = np.squeeze(results)
     top_k = results.argsort()[-5:][::-1]
     labels = load_labels(label_path)
-    response = list()
-    for i in top_k:
-        response.append({
+    return [{
             "name": labels[i], 
             "score": float(results[i])
-        })
-    return response
+            } for i in top_k]
 
 
 @hug.get('/finish', versions=1, output=hug.output_format.json)
 def predict_finish(image_url: hug.types.text):
     input_layer = "input"
     output_layer = "final_result"
-
     image_tensor = read_tensor_from_image_file(image_url,
                                 input_height=128,
                                 input_width=128,
                                 input_mean=128,
                                 input_std=128)
-    with MODELS['color-families'].as_default():
-        family_label_path = "model/color-families/labels.txt"
-        families = run_graph_v2(MODELS['color-families'], image_tensor, family_label_path)
-        selected_family = select_family(families)
+    family_label_path = "model/color-families/labels.txt"
+    families = run_graph_v2(MODELS['color-families'], image_tensor, family_label_path)
+    selected_family = select_family(families)
     if not selected_family:
         return {
             "color_families": [],
             "finishes": []
         }
-    finish_graph = tf.Graph()
     finishes = []
-    with MODELS[selected_family['name']].as_default():
-        if select_family:
-            finish_model_base = os.path.join("model", selected_family["name"])
-            finish_label_path = os.path.join(finish_model_base, "labels.txt")
-            finishes = run_graph_v2(MODELS[selected_family['name']], image_tensor, finish_label_path)
+    if selected_family:
+        finish_model_base = os.path.join("model", selected_family["name"])
+        finish_label_path = os.path.join(finish_model_base, "labels.txt")
+        finishes = run_graph_v2(MODELS[selected_family['name']], image_tensor, finish_label_path)
     return {
         "color_families": families,
         "finishes": finishes
